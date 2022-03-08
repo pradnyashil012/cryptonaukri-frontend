5a28d7b3b86d3a98900691a11da7403ce54d036b
sahiljena <sahiljena@protonmail.com>
2022-03-08 13:18:01 +0530

    added skeleton loading effect

--- src/components/JobsPage.jsx
+++ src/components/JobsPage.jsx
@@ -1,6 +1,7 @@
 import { Container, Box, makeStyles } from '@material-ui/core';
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import JobCard from './Job-Card/JobCard';
+import JobCardLoading from './Job-Card/JobCardLoading';
 import Axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
@@ -11,16 +12,20 @@ const useStyles = makeStyles((theme) => ({
 
 const JobsPage = (e) => {
     const classes = useStyles();
-    const [dataArr, setDataArr] = React.useState([]);
+    const [dataArr, setDataArr] = useState([]);
+    const [loading, setLoading] = useState(false);
     // const [datarr, setDataArr] = React.useState([]);
 
 
     useEffect(async ()=>{
+        setLoading(true);
         const response = await Axios.get('https://cryptonaukri-backend.herokuapp.com/jobs')
         .then((res)=>{
+            setLoading(false);
             setDataArr(res.data);
         })
         .catch((err)=>{
+            setLoading(false);
         });
     } ,[]);
 
@@ -31,12 +36,20 @@ const JobsPage = (e) => {
                     paddingTop: '20px'
                 }}>
                     <h1 style={{ margin: '10px 0' }}>Discover jobs</h1>
+                    {loading?
+                        <>
+                        <JobCardLoading />
+                        <JobCardLoading />
+                        <JobCardLoading />
+                        </>:<>
+                            {
+                                dataArr.slice(0).reverse().map((e) => {
+                                    return <JobCard position={e.position} cmp={e.company} exp={e.experience} opn={e.openings} link={e.link} srNo={e._id}/>
+                                })
+                            }
+                    </>}
 
-                    {
-                        dataArr.slice(0).reverse().map((e) => {
-                            return <JobCard position={e.position} cmp={e.company} exp={e.experience} opn={e.openings} link={e.link} srNo={e._id}/>
-                        })
-                    }
+                    
                 </Box>
             </Container>
         </div>
