6b0d4cc43c90b90071683793d8441dbc92a6027c
Dinesh Rout <rddinesh779@gmail.com>
2022-05-24 13:33:41 +0530

    minor fix

--- src/components/auth/DevLogin.js
+++ src/components/auth/DevLogin.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ForgotPassword from './ForgotPassword';
 import Axios from 'axios';
-<<<<<<< HEAD
 import { useNavigate } from 'react-router-dom';
 
 const Login = ({ setCookie }) => {
   const navigate = useNavigate();
+
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  var redirectType = url.searchParams.get('redirecttype');
+  var redirectid = url.searchParams.get('redirectid');
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -34,7 +38,7 @@ const Login = ({ setCookie }) => {
         if (data.userLoggedIn) {
           // console.log(data);
           console.log(response.headers.authorization);
-          setLoginError(data.message);
+          // setLoginError(data.message);
           setLoading(false);
           const timestamp = new Date().getTime();
           const expire = timestamp + 60 * 60 * 24 * 1000 * 3;
@@ -56,6 +60,15 @@ const Login = ({ setCookie }) => {
           localStorage.setItem('cUser', 'DEVELOPER');
           localStorage.setItem('login', true);
 
+          if (redirectType === 'internship' || redirectType === 'job') {
+            navigate(`/jobapplication?id=${redirectid}&type=${redirectType}`);
+            return;
+          }
+          if (redirectType === 'community') {
+            window.location.href = `https://community.cryptonaukri.com${redirectid}`;
+            return;
+          }
+
           navigate('/jobspage');
         }
 
@@ -68,110 +81,13 @@ const Login = ({ setCookie }) => {
           );
           setLoading(false);
         }
+        if (errResp.data.code === 'INVALID') {
+          setLoginError('Account has been disabled(free trial period expired)');
+          setLoading(false);
+        }
         if (errResp.data.code === 'WRONG_PASSWORD') {
           setLoginError('Wrong Email or Password');
           setLoading(false);
-=======
-import { useNavigate } from "react-router-dom";
-
-const Login = ({setCookie}) =>{
-
-    const navigate = useNavigate();
-
-    var url_string = window.location.href;
-    var url = new URL(url_string);
-    var redirectType = url.searchParams.get("redirecttype");
-    var redirectid = url.searchParams.get("redirectid");
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [loginError, setLoginError] = useState("");
-    const [loading, setLoading] = useState(false);
-    
-    // const [nPassWrongs, setNPassWrongs] = useState(0);
-
-
-    const API = process.env.REACT_APP_API_ENDPOINT;
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        //console.log(email.split('@')[1]);
-        // console.log(process.env.REACT_APP_PRODUCTION_API_ENDPOINT);
-        if(email && password){
-            setLoginError();
-            setLoading(true);
-
-            try {
-
-                const response = await Axios.post(`${API}/api/v1/user/login`, { email, password });
-                const data = response.data;
-                
-                if(data.userLoggedIn){
-                    // console.log(data);
-                    console.log(response.headers.authorization)
-                    // setLoginError(data.message);
-                    setLoading(false);
-                    const timestamp = new Date().getTime();
-                    const expire = timestamp + (60 * 60 * 24 * 1000 * 3);
-                    const expireDate = new Date(expire);
-
-                    try{
-                        setCookie(
-                            'token',response.headers.authorization,{
-                                expires: expireDate,
-                                path:'/',
-                                domain: '.cryptonaukri.com'
-                            }
-                        );
-                    }catch(error){
-                        alert(";p");
-                        console.log(error);
-                    }
-                    
-
-                    // below codes to be removed once cookies is applied accross the site
-                    localStorage.setItem('token', response.headers.authorization);
-                    localStorage.setItem('cUser', 'DEVELOPER');
-                    localStorage.setItem('login', true);
-
-                    if(redirectType==='internship' || redirectType==='job'){
-                        navigate(`/jobapplication?id=${redirectid}&type=${redirectType}`);
-                        return;
-                    }
-                    if(redirectType==='community'){
-                        window.location.href = `https://community.cryptonaukri.com${redirectid}`; 
-                        return;
-                    }
-
-                    navigate('/jobspage');
-                }
-
-                
-                
-                
-                //console.log(data);
-
-            }catch(error) {
-                const errResp = error.response;
-                if(errResp.data.code === 'NOT_FOUND'){
-                    setLoginError("You are not yet registered with us. Please Register your account");
-                    setLoading(false);
-                }
-                if(errResp.data.code === 'INVALID'){
-                    setLoginError("Account has been disabled(free trial period expired)");
-                    setLoading(false);
-                }
-                if(errResp.data.code === 'WRONG_PASSWORD' ){
-                    setLoginError("Wrong Email or Password");
-                    setLoading(false);
-                }
-                // setLoginError("Something went wrong!");
-                setLoading(false);
-            } 
-        }else{
-            setLoginError("Something went wrong!");
-            console.log("Email and password is required.")
->>>>>>> 92a92f3892127b5a491cb792aa5cf87093df2c5f
         }
         // setLoginError("Something went wrong!");
         setLoading(false);
