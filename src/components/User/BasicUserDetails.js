cb6ca4589fd79273fbaace1477d3ac8a6502585f
akshay782 <akshayk0782@gmail.com>
2022-07-08 14:47:04 +0530

    :sparkles: add: profile links

--- src/components/User/BasicUserDetails.js
+++ src/components/User/BasicUserDetails.js
@@ -1,20 +1,49 @@
 import { useState } from 'react';
 import EditProfile from './EditProfile'
+import {FiLinkedin, FiGithub, FiTwitter} from 'react-icons/fi'
+
 
 const BasicUserDetails = ({user, token}) =>{
 
     return(<>
     <div className="bg-gray-900 p-2 rounded">
         <div className="flex gap-2">
-            <div>
-                <img className={`w-20 h-20 rounded shadow-lg`} src={`https://avatars.dicebear.com/api/initials/${user?.firstName +" "+ user?.lastName}.svg`} />
+            <div className='w-20 h-20'>
+                <img className='w-full h-full rounded shadow-lg' src={`https://avatars.dicebear.com/api/initials/${user?.firstName +" "+ user?.lastName}.svg`} />
             </div>
-            <div className='gap-2'>
-                <span className={`text-md`}>{user?.firstName + " " +user?.lastName}</span><br/>
+            <div className='flex flex-col h-20 justify-between'>
+                <div>
+                    <div className='text-md'>{user?.firstName + " " +user?.lastName}</div>
+                    <div className='text-xs text-gray-200'>{user?.email}</div>
+                </div>
+                
+                <div className='flex flex-row justify-start'>
+                    {
+                        user.userResume.links.github === '' ? 
+                        null : 
+                        <a href={user.userResume.links.github} className='flex items-center'><FiGithub /></a>
+                    }
+                    {
+                        user.userResume.links.linkedin === '' ? 
+                        null : 
+                        <a href={user.userResume.links.linkedin} className='ml-3 flex items-end'><FiLinkedin /></a>
+                    }         
+
+                       {/* tell backend guy to include twitter field in schema */}
+                    {/* {
+                        user.userResume.links.twitter === '' ? 
+                        null : 
+                        <a href={user.userResume.links.linkedin} className='ml-3 flex items-end'><FiTwitter /></a>
+                    }                     */}
+                    
+                </div>
+
+                {/* <div>{user?.phoneNumber}</div>
+                <span className='text-md'>{user?.firstName + " " +user?.lastName}</span><br/>
                 <span className="text-xs text-gray-200">{user?.email}</span><br/>
                 <span className="text-xs text-gray-200">{user?.phoneNumber}</span><br/>
-                {/* <span className="text-xs text-gray-200">{user?.location}</span><br/> */}
-                {/* <span className="tetx-base text-blue-500">Community Rank : 130</span> */}
+                <span className="text-xs text-gray-200">{user?.location}</span><br/>
+                <span className="tetx-base text-blue-500">Community Rank : 130</span> */}
             </div>
         </div>
         
