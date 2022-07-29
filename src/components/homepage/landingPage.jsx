3fd5be55d64f795a55c06381551c751a401e5692
kridi32 <kridi32@outlook.com>
2022-07-29 22:25:15 +0600

    new landing page setup is done

--- src/components/homepage/landingPage.jsx
+++ src/components/homepage/landingPage.jsx
@@ -1,1 +1,12 @@
-// najmul landing page
+import React from 'react'
+import Header from './Components/Header'
+
+const landingPage = () => {
+  return (
+      <div>
+          <Header /> 
+    </div>
+  )
+}
+
+export default landingPage
