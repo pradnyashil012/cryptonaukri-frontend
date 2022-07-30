88d44a7a3e8735bd3f72de64c34d5627a27367d5
kridi32 <kridi32@outlook.com>
2022-07-30 14:26:52 +0600

    menu icon toggle for mobile device is done

--- src/components/homepage/Components/Header.jsx
+++ src/components/homepage/Components/Header.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import logo from '../Assets/logo.png'
 import { Link } from 'react-router-dom'
 import RegularButton from './RegularButton'
+import { useState } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import {ImCross} from 'react-icons/im'
 const Header = () => {
-  return (
-    <header className='w-full bg-[#00142A] py-3 shadow-md shadow-[#00142A]'>
+  const [menuDisplay, setMenuDisplay] = useState(false)
+  return ( 
+    <header className='w-full bg-[#00142A] py-3 shadow-lg '>
       <div className='container mx-auto flex justify-between items-center'>
-        <img src={logo} className='w-[300px]' alt="" />
-        <nav className='flex justify-end items-center text-white gap-4 font-medium'>
+        <img src={logo} className='w-[150px] md:w-[300px]' alt="" />
+        <nav className=' justify-end items-center text-white gap-4 font-medium hidden md:flex'>
           <Link to="#">Home</Link>
           <Link to="#">About</Link>
           <Link to="#">Community</Link>
@@ -15,7 +19,7 @@ const Header = () => {
           <Link to="#">Contact</Link>
           <RegularButton >Join Us</RegularButton>
         </nav>
-     
+        <h1 className="text-white md:hidden text-xl" onClick={() => setMenuDisplay(!menuDisplay)}>{menuDisplay? (<GiHamburgerMenu/>): (<ImCross/>)}</h1>
       </div>
     </header>
   )
--- src/components/homepage/landingPage.jsx
+++ src/components/homepage/landingPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './Components/Header'
-
+import heroImg from "./Assets/herobg.png"
 
 const landingPage = () => {
   return (
       <div className='testing-homepage'>
-          <Header /> 
+      <Header /> 
+      <main className='min-h-screen min-w-screen  bg-gradient-to-r from-[#000101] to-[#04162C] relative '> <img src={heroImg} className="absolute top-10 opaicity-30" alt='' /> <h1>Testing text</h1> </main>
     </div>
   )
 }
