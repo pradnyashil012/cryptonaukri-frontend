e7fbb784f2839d37f95e553e4c6b4258fd3a0e21
kridi32 <kridi32@outlook.com>
2022-08-01 18:34:35 +0600

    Hero left part is done

--- src/components/homepage/landingPage.jsx
+++ src/components/homepage/landingPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Header from './Components/Header'
-import heroImg from "./Assets/herobg.png"
+import Header from './Components/Header' 
 import Footer from './Components/Footer'
 import "./LandingPage.module.css"
+import Hero from './Components/Hero'
 const landingPage = () => {
   return (
       <div className='testing-homepage'>
       <Header /> 
-      <main className='min-h-screen min-w-screen  bg-gradient-to-r from-[#000101] to-[#04162C] relative '> <img src={heroImg} className="absolute top-10 opaicity-30" alt='' /> <h1>Testing text</h1> </main>
+      <Hero /> 
       <Footer /> 
     </div>
   )
